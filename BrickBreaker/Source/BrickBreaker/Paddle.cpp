393055db3fc2be848758ebf601dc153154433ae4
Sean Kelly <sevendaystillsean@gmail.com>
2017-07-02 19:02:09 -0700

    Left/Right movement of paddle started

---
+++ BrickBreaker/Source/BrickBreaker/Paddle.cpp
@@ -0,0 +1,67 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Paddle.h"
+
+
+// Sets default values
+APaddle::APaddle()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+    // Set this pawn to be controlled by the lowest-numbered player
+    AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+    // Create a dummy root component we can attach things to.
+    RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootComponent"));
+
+    // Create a paddle mesh
+    PaddleComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PaddleComponent"));
+    PaddleComponent->SetupAttachment(RootComponent);
+
+}
+
+// Called when the game starts or when spawned
+void APaddle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APaddle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+    // Handle movement based on our "MoveX" axes
+    if (!CurrentVelocity.IsZero())
+    {
+        FVector NewLocation = GetActorLocation() + (CurrentVelocity * DeltaTime);
+        SetActorLocation(NewLocation);
+    }
+
+}
+
+// Called to bind functionality to input
+void APaddle::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+    // Respond when our "fire" key is pressed or released.
+    InputComponent->BindAction("Fire", IE_Pressed, this, &APaddle::FireBall);
+
+    // Respond every frame to the values of our two movement axes, "MoveX" and "MoveY".
+    InputComponent->BindAxis("MoveHorizontal", this, &APaddle::Move_XAxis);
+
+}
+
+void APaddle::Move_XAxis(float AxisValue)
+{
+    // Move at 100 units per second forward or backward
+    CurrentVelocity.X = FMath::Clamp(AxisValue, -1.0f, 1.0f) * 100.0f;
+}
+
+void APaddle::FireBall()
+{
+}
+
---
+++ BrickBreaker/Source/BrickBreaker/Paddle.h
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Paddle.generated.h"
+
+UCLASS()
+class BRICKBREAKER_API APaddle : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	APaddle();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+    //Input functions
+    void Move_XAxis(float AxisValue);
+    void FireBall();
+
+
+
+    UPROPERTY(EditAnywhere)
+    USceneComponent* PaddleComponent;
+
+
+    //Input variables
+    FVector CurrentVelocity;
+};
